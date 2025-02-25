a54d639bd5177ddb723707e50c9b33131bc34312
Sergey Shiryev <grayyyyy456@yandex.ru>
2025-02-25 16:49:42 +0300

    Добавлены декораторы step для всех методов в файле home_page.py

--- pages/home_page.py
+++ pages/home_page.py
@@ -1,34 +1,43 @@
 from pages.base_page import BasePage
 from locators.home_page_locators import Locators
+import allure
 
 
 
 class HomePageMesto(BasePage):
-
+    @allure.step("Ожидание загрузки вопросов")
     def wait_for_load_questions(self):
         self.wait_for_element(Locators.questions, timeout=5)
 
+    @allure.step('Кликаем по первому вопросу')
     def click_first_question(self):
         self.click_element(Locators.question_1)
 
+    @allure.step("Кликаем по второму вопросу")
     def click_second_question(self):
         self.click_element(Locators.question_2)
 
+    @allure.step("Кликаем по третьему вопросу")
     def click_third_question(self):
         self.click_element(Locators.question_3)
 
+    @allure.step("Кликаем по четвёртому вопросу")
     def click_fourth_question(self):
         self.click_element(Locators.question_4)
 
+    @allure.step("Кликаем по пятому вопросу")
     def click_fifth_question(self):
         self.click_element(Locators.question_5)
 
+    @allure.step("Кликаем по шестому вопросу")
     def click_sixth_question(self):
         self.click_element(Locators.question_6)
 
+    @allure.step("Кликаем по седьмому вопросу")
     def click_seventh_question(self):
         self.click_element(Locators.question_7)
 
+    @allure.step("Кликаем по восьмому вопросу")
     def click_eighth_question(self):
         self.click_element(Locators.question_8)
 
